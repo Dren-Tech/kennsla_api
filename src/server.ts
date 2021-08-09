8057ce0a5a4a98e723508107e254eb260bc622a3
Marian Hahne <marian.hahne@outlook.com>
2021-08-09 20:53:05 +0200

    Add express server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,22 @@
-import { flaschenpost } from 'flaschenpost';
+import express from 'express';
+import { flaschenpost, getMiddleware } from 'flaschenpost';
 
 const logger = flaschenpost.getLogger();
 
+logger.info('Booting content service...');
+
+const app = express();
+const port = 8080;
+
+app.use(getMiddleware({ logOn: 'request' }));
+
+app.get('/', (request, response): any => {
+  response.send('Express + TypeScript Server');
+});
+
 logger.info('Start content service of Kennlsa');
+
+// Start server
+app.listen(port, (): void => {
+  logger.info(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
