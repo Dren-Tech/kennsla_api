36eb0b1619b271cc474220bceb2ece269620ac7f
Marian Hahne <marian.hahne@outlook.com>
2021-08-09 20:34:48 +0200

    Add flaschenpost and nodemon

--- server.ts
+++
@@ -1,1 +0,0 @@
-console.log('Start Kennsla service content-service');
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import { flaschenpost } from 'flaschenpost';
+
+const logger = flaschenpost.getLogger();
+
+logger.info('Start content service of Kennlsa');
