5059c0b405820ab284396a21191bea12eae830f8
Marian Hahne <marian.hahne@outlook.com>
2021-08-10 20:14:34 +0200

    Add GraphQL endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
+import { buildSchema } from 'graphql';
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
 import { flaschenpost, getMiddleware } from 'flaschenpost';
 
 const logger = flaschenpost.getLogger();
@@ -6,17 +8,45 @@ const logger = flaschenpost.getLogger();
 logger.info('Booting content service...');
 
 const app = express();
-const port = 8080;
+const port = 8_080;
 
+// --------------
+// Express config
+// --------------
 app.use(getMiddleware({ logOn: 'request' }));
 
 app.get('/', (request, response): any => {
   response.send('Express + TypeScript Server');
 });
 
+// --------------
+// GraphQL config
+// --------------
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello (): string {
+    return 'Hello world!';
+  }
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true
+}));
+
 logger.info('Start content service of Kennlsa');
 
 // Start server
 app.listen(port, (): void => {
   logger.info(`⚡️[server]: Server is running at http://localhost:${port}`);
+  logger.info(`⚡️[server]: GraphQL endpoint is running at http://localhost:${port}/graphql`);
 });
